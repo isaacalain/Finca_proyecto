3c21219e99c665566fa1d065b9430c649fe3ee6c
Isaac <isaacalain5@gmail.com>
2024-07-13 13:36:12 -0600

    Avance 1

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php require_once "./parte_superior.php" ?>
+<div class="titulo">
+    <p>Pasar unas vacaciones en Puerto Viejo es una gran decisión.    ¿Por que no un buen hospedaje?</p>
+    
+</div>
+
+
+
+<body>
+    <div class="main-img">
+        <img src="./images/playa.jpg" alt="">
+
+    </div>
+
+    <div class="main-accordion-container"></div>
+    <div class="mid-info">
+        <p class="mid-info-title">Bungalos</p>
+        <p class ="mid-info-desc">Decide donde pasar tus vacaciones y salir de la rutina</p>
+    </div>
+    <div class="card-list">
+
+    </div>
+
+
+</body>
+
+
+
+<?php require_once "./parte_inferior.php" ?>
---
+++ parte_superior.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="English">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Poyecto de Tercera 1" content="Página de prueba">
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+<menu>
+    <div class="menu-izquierda">
+        <a href="./index.php">
+            <img src="./images/IMG-20240713-WA0011.jpg" alt="">
+        </a>
+
+    </div>
+    <div class="menu-container">
+        <div class="menu-derecha">
+            <a class="enlaces-menu-derecha" href="https://google.com">Cotizar</a>
+            <a class="enlaces-menu-derecha" href="https://google.com">Contactanos</a>
+
+        </div>
+        <div class ="hamburger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+</menu>
